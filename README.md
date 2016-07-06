58e5dc3c0d7f339d7daddc4d28a8cf89426751fa
Songkijung <skj4522@gmail.com>
2016-07-06 22:59:53 +0900

    helloExpress.js 주석 추가 및 README.md 파일 업데이트

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # nodejs-study
+## 개발환경에 따라 IP주소와 PORT값이 다를수 있기때문에 serverinfo.js 파일을 통해 서버정보를 관리하고 있습니다. serverinfo.templet.js파일을 복사해서 serverinfo.js 파일을 생성하시면 됩니다.
--- helloExpress.js
+++ helloExpress.js
@@ -1,11 +1,17 @@
-const serverinfo = require('./serverinfo');
-var express = require('express');
-var app = express();
+const serverinfo = require('./serverinfo'); // 서버정보(ip,port)정보를 가진 .js파일
+var express = require('express');           // express 라이브러리를 import한다.
+var app = express();                        // express를 생성하여 app 변수에 저장.
 
+// serverinfo.port에 설정한 포트값으로 listen 한다.
+// listening이 성공하면  두번째 인자인 콜백 메서드를 호출한다.
 app.listen(serverinfo.port, function(){
     console.log('Connected!!!');
 })
+})
 
+// get 라우팅을 설정하고 해당 1번째 인자로 전달한 path에 접속 시,
+// 두번째 인자 콜백 메서드를 호출하여 결과값을 보내준다.
 app.get('/', function(req, res){
   res.send("hello express!!");
+})
 })
